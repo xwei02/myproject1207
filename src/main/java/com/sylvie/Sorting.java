2e10b3a9c674bfabd033035f2805ca05de9bcce9
USER <sylvie.wu@hotmail.com>
2021-12-07 14:44:26 +0800

    commit

---
+++ src/main/java/com/sylvie/Sorting.java
@@ -0,0 +1,28 @@
+package com.sylvie;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] n = {12,5, 6, 3, 13};
+        System.out.println(n.length);
+        for (int i = 0; i < n.length-1; i++) {
+            System.out.println("i"+1);
+            for (int j = i+1; j < n.length; j++) {
+                System.out.print("  "+j+",");
+                if (n[i] < n[j]){
+                    int tmp = n[i];
+                    n[i] = n[j];
+                    n[j] = tmp;
+
+                }
+
+            }
+            System.out.println();
+
+        }
+        for (int i = 0; i < n.length; i++) {
+            System.out.print(n[i]+",");
+
+        }
+
+    }
+}
